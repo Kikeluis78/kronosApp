961d1ea0e738adae102c499400e3c0b26c358b99
EnriqueVargas <enriquevargas078@gmail.com>
2025-08-26 21:03:53 -0600

    Fix import LogoPrincipal case sensitivity

--- src/components/utilidades/Logoprincipal.jsx
+++ src/components/utilidades/Logoprincipal.jsx
@@ -1,4 +1,6 @@
 // src/components/utilidades/LogoPrincipal.jsx
+'use client';
+
 export default function LogoPrincipal() {
   return (
     <div className="my-6 text-center border-2 border-indigo-500 rounded-lg p-4 neon-glow">
